fc8077a3b021ccfe9129e4ca5b6d56e6233c2c68
Jakub <jakubstefaniak99@gmail.com>
2023-12-26 11:42:21 +0100

    XOR - implementacja
    Szyfrowanie pliku działa poprawnie, dalsza część kodu jeszcze do poprawki.
    
    Zmiana również metody szyfrowania na XOR

--- main.py
+++ main.py
@@ -1,43 +1,65 @@
 import os
-import management
+import io
+from flask import Flask, render_template, request, send_file, jsonify
+
+app = Flask(__name__)
 
 def xor_encrypt_decrypt(data, key):
+    key_bytes = key.encode()
     encrypted_data = bytearray()
     for i in range(len(data)):
         data_byte = data[i]
-        key_byte = key[i % len(key)]
+        key_byte = key_bytes[i % len(key_bytes)]
         encrypted_data.append(data_byte ^ key_byte)
     return bytes(encrypted_data)
 
-def encrypt_decrypt_file(file_path, key):
-    with open(file_path, 'rb') as file:
-        data = file.read()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        print("Form data:", request.form)    # Debugowanie
+        print("Files data:", request.files)  
+        
+        key_path = request.form.get('key_path')
+        file = request.files.get('file')
+        file = request.files['file'].filename
+        print(key_path, file)
 
-    processed_data = xor_encrypt_decrypt(data, key)
+        if not key_path or not file:
+            return "Klucz jest wymagany", 400
+        
+        try:
+            with open(key_path, 'rb') as key_file:
+                key_data = key_file.read()
+        except IOError:
+            return "Nie można odczytać klucza", 400
+        
+        operation = request.form.get('operation')
 
-    with open(file_path, 'wb') as file:
-        file.write(processed_data)
+        if 'file' not in request.files or request.files['file'].filename == '':
+            return "Plik jest wymagany", 400
 
-generate_key_decision = input("Czy chcesz wygenerować nowy klucz? (tak/nie): ")
-if generate_key_decision.lower() == 'tak':
-    key = management.generate_key()
-else:
-    key = management.load_key()
+        uploaded_file = request.files['file']
 
-# Interfejs użytkownika
-print("1: Szyfrowanie pliku")
-print("2: Rozszyfrowanie pliku")
-choice = input("Wybierz opcje (1/2): ")
+        try:
+            if operation == 'encrypt':
+                processed_data = xor_encrypt_decrypt(uploaded_file.read(), file)
+                filename = 'encrypted_file'
+            elif operation == 'decrypt':
+                processed_data = xor_encrypt_decrypt(uploaded_file.read(), file)
+                filename = 'decrypted_file'
+            else:
+                return "Niepoprawna operacja", 400
 
-if choice not in ['1', '2']:
-    print("Nieprawidłowy wybór. Proszę wybrać 1 lub 2.")
-else:
-    file_path = input("Proszę wprowadzić lokalizację pliku: ")
-    try:
-        if os.path.exists(file_path):
-            encrypt_decrypt_file(file_path, key)
-        else:
-            print("Plik nie istnieje.")
-    except IOError as e:
-        print(f"Wystąpił błąd podczas dostępu do pliku: {e}")
-  
+            return send_file(
+                io.BytesIO(processed_data),
+                download_name=file,
+                as_attachment=True
+            )
+        except Exception as e:
+            print("Błąd:", str(e))  # Debugowanie
+            return jsonify({"error": str(e)}), 500
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+     app.run(debug=True)
--- management.py
+++ management.py
@@ -1,14 +1,50 @@
+from flask import Flask, request, jsonify
 import os
 
-def generate_key(length=16):  # długość klucza
-    key = os.urandom(length)
-    key_path = input("Proszę wprowadzić lokalizację zapisu klucza (np. H:\klucze): ")
-    os.makedirs(os.path.dirname(key_path), exist_ok=True)
-    with open(key_path, "wb") as key_file:  # Zapisz klucz jako plik binarny
-        key_file.write(key)
-    return key
+app = Flask(__name__)
 
-def load_key():
-    key_path = input("Proszę wprowadzić lokalizację wczytania klucza (np. H:\klucze): ")
-    with open(key_path, "rb") as key_file:
-        return key_file.read()
+@app.route('/', methods=['POST'])
+def key_operations():
+    print(request.form)
+    print("Files data:", request.files)
+    try:
+        operation = request.form.get('operation')
+        # Tworzenie pliku
+        if operation == 'generate':
+            length = request.form.get('length', default=16, type=int)
+            key_path = request.form.get('key_path')
+
+            # plik teksowy
+            if not key_path.endswith('.txt'):
+                key_path += '.txt'
+
+            os.makedirs(os.path.dirname(key_path), exist_ok=True)
+
+            key = os.urandom(length)
+            with open(key_path, "wb") as key_file:
+                key_file.write(key)
+
+            return jsonify({"message": "Klucz został wygenerowany.", "key_path": key_path})
+        # Wczytanie pliku
+        elif operation == 'load':
+            if 'file' not in request.files:
+                return jsonify({"error": "Brak wymaganego pliku w zapytaniu"})
+
+            key_file = request.files['file']
+            if key_file.filename == '':
+                return jsonify({"error": "Nie wybrano pliku"})
+
+            key = key_file.read()  # Read key directly from the uploaded file
+            return jsonify({"message": "Klucz został wczytany.", "key": key.hex()})
+
+        else:
+            return jsonify({"error": "Niepoprawna operacja."})
+
+    except FileNotFoundError:
+        return jsonify({"error": "Plik klucza nie istnieje."})
+    except Exception as e:
+        print("Error:", str(e))
+    return jsonify({"error": str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
